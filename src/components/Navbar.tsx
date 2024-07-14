2071e34370207a3e6c85b7d430da773eef4725ca
folka2134 <folka2134@gmail.com>
2024-07-14 22:44:29 +0100

    implemented clerk signin

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,22 @@
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/clerk-react";
+
 const Navbar = () => {
   return (
     <div>
       <ul className="flex justify-between px-24">
         <span>LOGO</span>
         <div className="flex gap-3">
-          <span>Login</span>
-          <span>Sign-up</span>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
         </div>
       </ul>
     </div>
